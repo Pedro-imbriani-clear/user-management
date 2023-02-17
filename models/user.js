3b61c89971ddd7d55a2b94def8a482d8249bb04e
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-02-17 11:34:43 -0300

    refactoring

---
+++ models/user.js
@@ -0,0 +1,14 @@
+class User {
+constructor(name, gender,birth, country, email,admin,photo,password){
+
+    this.name = name;
+    this.gender = gender;
+    this.birth = birth;
+    this.country = country;
+    this.password = password;
+    this.photo = photo;
+    this.admin = admin;
+    this.email = email;
+}
+
+}
